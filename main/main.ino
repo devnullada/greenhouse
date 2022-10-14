7e182860a6074b79427cdd9aaf458ae17c2752d0
Pål Thingbø <pth@live.no>
2022-10-14 15:24:19 +0200

    Stuff

--- main/main.ino
+++ main/main.ino
@@ -6,29 +6,17 @@ DHT dht(DHTPIN, DHTTYPE);
 
 // CONFIG
 boolean debug = false;
-unsigned long waitSeconds = 60;
-float tempThreshold = 21;
-unsigned long heatSeconds = 5;
+unsigned long waitSeconds = 40;
+float tempThreshold = 22;
+unsigned long heatSeconds = 6;
 // END CONFIG
 
 int pinHeater = 11;
-bool isHeating = false;
-unsigned long lastStarted;
-unsigned long lastStopped;
 
 
 void setup() {
   pinMode(pinHeater, OUTPUT);
-
   digitalWrite(pinHeater, LOW);
-  delay(1000);
-  digitalWrite(pinHeater, HIGH);
-  delay(5000);
-  digitalWrite(pinHeater, LOW);
-  delay(1000);
-
-  lastStopped = millis() - (waitSeconds * 1000);
-  lastStarted = millis() - (waitSeconds * 1000) - (heatSeconds * 1000);
 
   dht.begin();
 
@@ -38,29 +26,19 @@ void setup() {
 }
 
 void loop() {
-  delay(1000);
   float celsius = dht.readTemperature();
-  unsigned long stoppedSeconds = (millis() - lastStopped) / 1000;
   
-  long addSeconds = (7 - (celsius / 2));
-  if (addSeconds < 0) addSeconds = 0;
-  if (addSeconds > 5) addSeconds = 5;
-
-  
-
-  if (
-      !isHeating && 
-      celsius < tempThreshold &&
-      stoppedSeconds > waitSeconds) {
-    heatSeconds = 5 + addSeconds;    
-    startHeater();
+  if (celsius < tempThresholds) {
+    long addSeconds = (7 - (celsius / 2));
+    if (addSeconds < 0) addSeconds = 0;
+    if (addSeconds > 5) addSeconds = 5;  
+    long onSeconds = heatSeconds + addSeconds;    
+    digitalWrite(pinHeater, HIGH);
+    delay(onSeconds * 1000);
+    digitalWrite(pinHeater, LOW);
   }
 
-  unsigned long runningSeconds = (millis() - lastStarted) / 1000;
-
-  if (isHeating && runningSeconds > heatSeconds) {
-    stopHeater();
-  }
+  delay(waitSeconds * 1000);
 
   if (debug) {
     Serial.print(F("Celsius: "));
@@ -74,25 +52,4 @@ void loop() {
     Serial.print(F("s"));
     Serial.print(F("\n "));
   }
-}
-
-
-void startHeater() {
-  digitalWrite(pinHeater, HIGH);
-  isHeating = true;
-  lastStarted = millis();
-  if (debug) {
-    Serial.print(F("START\n"));
-  }
-  delay(1000);
-}
-
-void stopHeater() {
-  digitalWrite(pinHeater, LOW);
-  isHeating = false;
-  lastStopped = millis();
-  if (debug) {
-    Serial.print(F("STOP\n"));
-  }
-  delay(1000);
 }
