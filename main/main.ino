e3738513e497e6ec7eeaf8032190221eb3e59ec4
Pål Thingbø <pth@live.no>
2022-10-23 21:22:38 +0200

    Stuff

--- main/main.ino
+++ main/main.ino
@@ -5,10 +5,10 @@
 DHT dht(DHTPIN, DHTTYPE);
 
 // CONFIG
-boolean debug = false;
-unsigned long waitSeconds = 40;
+boolean debug = true;
+unsigned long waitSeconds = 30;
 float tempThreshold = 22;
-unsigned long heatSeconds = 6;
+unsigned long heatSeconds = 15;
 // END CONFIG
 
 int pinHeater = 11;
@@ -28,11 +28,21 @@ void setup() {
 void loop() {
   float celsius = dht.readTemperature();
   
-  if (celsius < tempThresholds) {
+  if (celsius < tempThreshold) {
     long addSeconds = (7 - (celsius / 2));
     if (addSeconds < 0) addSeconds = 0;
     if (addSeconds > 5) addSeconds = 5;  
     long onSeconds = heatSeconds + addSeconds;    
+
+    if (debug) {
+      Serial.print(F("onSeconds: "));
+      Serial.print(onSeconds);
+      Serial.print(F("\n "));
+      Serial.print(F("addSeconds: "));
+      Serial.print(addSeconds);
+      Serial.print(F("\n "));
+    }
+    
     digitalWrite(pinHeater, HIGH);
     delay(onSeconds * 1000);
     digitalWrite(pinHeater, LOW);
@@ -43,13 +53,6 @@ void loop() {
   if (debug) {
     Serial.print(F("Celsius: "));
     Serial.print(celsius);
-    Serial.print(F("heatSeconds: "));
-    Serial.print(heatSeconds);
-    Serial.print(F("°C. Stopped: "));
-    Serial.print(stoppedSeconds);
-    Serial.print(F("s. Running: "));
-    Serial.print(runningSeconds);
-    Serial.print(F("s"));
     Serial.print(F("\n "));
   }
 }
